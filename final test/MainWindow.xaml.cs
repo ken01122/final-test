acd1ef7e8620c20824fae3c1755ee371f400e991
易軒 吳 <ken99877@gmail.com>
2021-01-05 16:22:38 +0800

    ui de

--- final test/MainWindow.xaml.cs
+++ final test/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +25,16 @@ namespace final_test
         {
             InitializeComponent();
         }
+    }
+    public class treenode 
+    {
+        public string titel { get; set; }
+        public ObservableCollection<treenode> Item { get; set; }
+    
+    }
+    public class student
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
     }
 }
