40957a4c2143c6c10e46a0a595064b8f204712a8
易軒 吳 <ken99877@gmail.com>
2021-01-07 17:19:01 +0800

    data in

--- final test/MainWindow.xaml.cs
+++ final test/MainWindow.xaml.cs
@@ -1,18 +1,11 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration.Attributes;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
+using System.Globalization;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace final_test
 {
@@ -23,9 +16,34 @@ namespace final_test
     {
         public MainWindow()
         {
+            List<student> studentlist = new List<student>();
+            List<course> courselist = new List<course>();
             InitializeComponent();
+            using (var reader = new StreamReader("D:\\2B.csv",Encoding.Default))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<student>();
+                foreach(var studentdata in records)
+                {
+                    studentlist.Add(studentdata);
+                }
+            }
+            comboBox.ItemsSource = studentlist;
+
+            using (var reader = new StreamReader("D:\\course.csv", Encoding.Default))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<course>();
+                foreach (var coursedata in records)
+                {
+                    courselist.Add(coursedata);
+                }
+            }
+            listView1.ItemsSource = courselist;
         }
+
     }
+    
     public class treenode 
     {
         public string titel { get; set; }
@@ -34,7 +52,35 @@ namespace final_test
     }
     public class student
     {
+        [Index(0)]
         public string ID { get; set; }
+        [Index(1)]
         public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{ this.ID} { this.Name}";
+        }
+    }
+    public class course
+    {
+        [Index(0)]
+        public string Teacher { get; set; }
+        [Index(1)]
+        public string CourseName { get; set; }
+        [Index(2)]
+        public string CourseNum { get; set; }
+        [Index(3)]
+        public string CourseMoN { get; set; }
+        [Index(4)]
+        public string CourseClass { get; set; }
+        [Index(5)]
+        public string CourseTime { get; set; }
+       
+
+        public override string ToString()
+        {
+            return $"{ this.Teacher} { this.CourseName} { this.CourseNum} { this.CourseMoN}" +
+                $"{ this.CourseClass} { this.CourseTime}";
+        }
     }
 }
