aaaeab9c3014e59d26f0e13145e19afcb71f4c37
易軒 吳 <ken99877@gmail.com>
2021-01-13 02:14:29 +0800

    all none

--- final test/MainWindow.xaml.cs
+++ final test/MainWindow.xaml.cs
@@ -17,18 +17,14 @@ namespace final_test
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<coursesel> coursesellist = new List<coursesel>();
+        List<record> coursesellist = new List<record>();
         List<student> studentlist = new List<student>();
         List<course> courselist = new List<course>();
         List<teacher> teacherlist = new List<teacher>();
-
+        List<print> printlist = new List<print>();
+        int selectedclassnum=0,selectedclasspoint=0;
         public MainWindow()
-        {
-            
-           
-
-
-
+        {                  
             InitializeComponent();
             using (var reader = new StreamReader("D:\\2B.csv",Encoding.Default))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -41,7 +37,7 @@ namespace final_test
             }
             comboBox.ItemsSource = studentlist;
             string sat= " ";
-            int i = -1;
+            int i = -1, j = 0; ;
             using (var reader = new StreamReader("D:\\course.csv", Encoding.Default))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {                
@@ -54,13 +50,15 @@ namespace final_test
                     {
                         sat = coursedata.Teacher;
                         treeset.TeacherName = coursedata.Teacher;
-                        treeset.TeacherCourse.Add(new courses() { tea=coursedata.Teacher,coursename = coursedata.CourseName, courseclass = coursedata.CourseClass, type = coursedata.Type, point = coursedata.Point});
+                        treeset.TeacherCourse.Add(new courses() { code=j,tea=coursedata.Teacher,coursename = coursedata.CourseName, courseclass = coursedata.CourseClass, type = coursedata.Type, point = coursedata.Point});
                         teacherlist.Add(treeset);
                         i += 1;
+                        j += 1;
                     }
                     else
                     {
-                        teacherlist[i].TeacherCourse.Add(new courses() { tea = coursedata.Teacher, coursename = coursedata.CourseName, courseclass = coursedata.CourseClass, type = coursedata.Type, point = coursedata.Point });
+                        teacherlist[i].TeacherCourse.Add(new courses() { code = j,tea = coursedata.Teacher, coursename = coursedata.CourseName, courseclass = coursedata.CourseClass, type = coursedata.Type, point = coursedata.Point });
+                        j += 1;
                     }
 
                 }
@@ -70,43 +68,200 @@ namespace final_test
             }
             listView1.ItemsSource = courselist;
             treeView.ItemsSource = teacherlist;
-
+            
            
         }
 
         private void treeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            label1.Content = "教師姓名:" + treeView.SelectedItem;
-            
-
-        
+        {      
+            if (treeView.SelectedItem is courses)
+            {
+                label1.Content = treeView.SelectedItem;
+            }        
         }
 
         private void selectbutton_Click(object sender, RoutedEventArgs e)
-        {            
-           
+        {
+            NewMethod();
+            aaaaa();
+        }
+
+        private void aaaaa()
+        {
+            if (comboBox.SelectedItem != null && listView1.SelectedItem != null)
+            {
+                student selstd = new student()
+                {
+                    ID = studentlist[comboBox.SelectedIndex].ID,
+                    Name = studentlist[comboBox.SelectedIndex].Name
+
+                };
+                courses cos = new courses()
+                {
+                    coursename = courselist[listView1.SelectedIndex].CourseName,
+                    courseclass = courselist[listView1.SelectedIndex].CourseClass,
+                    tea = courselist[listView1.SelectedIndex].Teacher,
+                    type = courselist[listView1.SelectedIndex].Type,
+                    point = courselist[listView1.SelectedIndex].Point,
+                    code = listView1.SelectedIndex
+
+
+                };
+                record rds = new record()
+                {
+                    selstudent = selstd,
+                    selcourse = cos
+                };
+
+                if (coursesellist.Count > 0)
+                {
+                    int b = 0;
+                    for (int x = 0; x < coursesellist.Count; x++)
+                    {
+                        if (coursesellist[x].selcourse.code == rds.selcourse.code)
+                        {
+                            b += 1;
+                        }
+                        if (coursesellist[x].selcourse.code != rds.selcourse.code && x == coursesellist.Count - 1 && b == 0)
+                        {
+                            coursesellist.Add(rds);
+                            selectedclassnum += 1;
+                            selectedclasspoint += cos.point;
+                        }
+                    }
+                }
+                else
+                {
+                    coursesellist.Add(rds);
+                    selectedclassnum += 1;
+                    selectedclasspoint += cos.point;
+                }
+                listView.ItemsSource = coursesellist;
+                listView.Items.Refresh();
+            }
+        }
+
+        private void NewMethod()
+        {
+            if (comboBox.SelectedItem != null && treeView.SelectedItem != null)
+            {
+                student selstd = new student()
+                {
+                    ID = studentlist[comboBox.SelectedIndex].ID,
+                    Name = studentlist[comboBox.SelectedIndex].Name
+
+                };
+                record rd = new record()
+                {
+                    selstudent = selstd,
+                    selcourse = treeView.SelectedItem as courses
+                };
+
+                if (coursesellist.Count > 0)
+                {
+                    int b = 0;
+                    for (int x = 0; x < coursesellist.Count; x++)
+                    {
+                        if (coursesellist[x].selcourse.code == rd.selcourse.code)
+                        {
+                            b += 1;
+                        }
+                        if (coursesellist[x].selcourse.code != rd.selcourse.code && x == coursesellist.Count - 1 && b == 0)
+                        {
+                            coursesellist.Add(rd);
+                            selectedclassnum += 1;
+                            selectedclasspoint += rd.selcourse.point;
+                        }
+                    }
+                }
+                else
+                {
+                    coursesellist.Add(rd);
+                    selectedclassnum += 1;
+                    selectedclasspoint += rd.selcourse.point;
+                }
+                listView.ItemsSource = coursesellist;
+                listView.Items.Refresh();
+            }
+            
+        }
+
+        private void deletebutton_Click(object sender, RoutedEventArgs e)
+        {      
+            coursesellist.RemoveAt(listView.SelectedIndex);
+            selectedclassnum -= 1;
+            selectedclasspoint -= courselist[listView1.SelectedIndex].Point;
+        }
+
+        private void comboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            coursesellist.Clear();
+            listView.Items.Refresh();
+            selectedclassnum = 0;
+            selectedclasspoint = 0;
+
+
+        }
+
+        private void listView1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+                                
+            label1.Content = listView1.SelectedItem;
+            
+                        
+        }
+
+        private void savebutton_Click(object sender, RoutedEventArgs e)
+        {
+            print pn = new print()
+            {
+                學號 = studentlist[comboBox.SelectedIndex].ID,
+                姓名 = studentlist[comboBox.SelectedIndex].Name,
+                已選課數 = selectedclassnum,
+                已選學分數 = selectedclasspoint
+            };
+
+            using (var writer = new StreamWriter($"C:\\Users\\Administrator\\Desktop\\{comboBox.SelectedItem}選課清單.csv",false,Encoding.UTF8))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                printlist.Add(pn);
+                csv.WriteRecords(printlist);
+            }
+
+            printlist.Clear();
         }
     }
+    class print
+    {
+        public string 學號 { get; set; }
+        public string 姓名 { get; set; }
+        public int 已選課數{ get; set; }
+        public int 已選學分數 { get; set; }
+    }
+    class record
+    {
+        public student selstudent { get; set; }
+        public courses selcourse { get; set; }
 
-
-    public class courses
+    }
+    class courses
     {
         public string tea { get; set; }
         public string coursename { get; set; }
         public string courseclass { get; set; }
-        public string point { get; set; }
+        public int point { get; set; }
         public string type { get; set; }
+        public int code { get; set; }
+       
         public override string ToString()
         {
             return $"{this.tea} { this.coursename} {this.type} { this.point} {"學分"} {"開課班級:"}{ this.courseclass}";
         }
 
-    }
-        
+    }      
 
-    public class teacher
-    {
-        
+    class teacher
+    {        
         public string TeacherName { get; set; }
         public ObservableCollection<courses> TeacherCourse { get; set; } 
         public teacher()
@@ -120,7 +275,7 @@ namespace final_test
         }
 
     }
-     class student
+    class student
     {
         [Index(0)]
         public string ID { get; set; }
@@ -138,29 +293,20 @@ namespace final_test
         [Index(1)]
         public string CourseName { get; set; }
         [Index(2)]
-        public string Point { get; set; }
+        public int Point { get; set; }
         [Index(3)]
         public string Type { get; set; }
         [Index(4)]
         public string CourseClass { get; set; }
         [Index(5)]
         public string CourseTime { get; set; }
+       
+        
         public override string ToString()
         {
             return $"{ this.Teacher} { this.CourseName}  { this.Point} { this.Type}" +
                 $"{ this.CourseClass} { this.CourseTime}";
         }
     }
-    public class coursesel
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Teacher { get; set; }
-        public string CourseName { get; set; }        
-        public string Point { get; set; }       
-        public string Type { get; set; }      
-        public string CourseClass { get; set; }
-        
 
-    }
 }
